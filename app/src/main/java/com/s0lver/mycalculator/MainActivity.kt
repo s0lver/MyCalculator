3ffc8d67f129d2a9da769184cc1e3f795c4b141c
Rafael Perez <rperez.github@gmail.com>
2026-02-18 21:20:04 -0800

    Early implementation for the classic calculator
    - By now it simply concatenates the number in screen
    - We will need states / FSM to know when to append to number in screen
      or when to start a new number.
    - We need also validation when clicking dot twice, etc

--- app/src/main/java/com/s0lver/mycalculator/MainActivity.kt
+++ app/src/main/java/com/s0lver/mycalculator/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
@@ -94,14 +95,92 @@ class MainActivity : ComponentActivity() {
                         }
                     }
 
-                    Spacer(modifier = Modifier.height(32.dp))
+                    Spacer(modifier = Modifier.height(16.dp))
 
                     Row {
                         Text("Result is: ", modifier = Modifier.align(Alignment.CenterVertically))
                         Text(result.toString())
                     }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    // 0 means you are in number state
+                    // 1 means you are in operator state
+                    var currentStatus by remember { mutableStateOf(0) }
+                    var currentNumber by remember { mutableStateOf("0") }
+
+                    Text("Calculator v2")
+                    Text(currentNumber)
+
+
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "7")
+                        }) { Text("7") }
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "8")
+                        }) { Text("8") }
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "9")
+                        }) { Text("9") }
+                        Button(onClick = {}) { Text("+") }
+                    }
+
+                    Row {
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "4")
+                        }) { Text("4") }
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "5")
+                        }) { Text("5") }
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "6")
+                        }) { Text("6") }
+                        Button(onClick = {}) { Text("-") }
+                    }
+
+                    Row {
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "1")
+                        }) { Text("1") }
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "2")
+                        }) { Text("2") }
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "3")
+                        }) { Text("3") }
+                        Button(onClick = {}) { Text("*") }
+                    }
+
+                    Row {
+                        Button(onClick = {
+                            currentNumber = appendNumber(currentNumber, "0")
+                        }) { Text("0") }
+                        Button(onClick = {}) { Text(".") }
+                        Button(onClick = {}) { Text("=") }
+                        Button(onClick = {}) { Text("/") }
+                    }
                 }
             }
         }
     }
+}
+
+enum class CalculatorState {
+    NUMBER,
+    OPERATOR
+}
+
+enum class CalculatorOperator {
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE
+}
+
+fun appendNumber(currentNumber: String, newNumber: String): String {
+    if (currentNumber == "0") {
+        return newNumber
+    }
+    return currentNumber + newNumber
 }
